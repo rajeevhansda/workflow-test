6e7fca8ab7e70251d479d89f2c52fc001b9c5132
Rajeev Hansda <126853105+rajeevhansda@users.noreply.github.com>
2023-06-15 06:08:57 +0000

    add to text

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 console.log('hello');
+const fs = require('fs'); 
 require("dotenv").config();
+
+ 
+
+ 
 
 if(process.env.KEY === 'mykey'){
     console.log('dotenv');
+    fs.appendFile('example.txt', 'FOUND DOTENV.', (err) => { 
+        if (err) throw err; 
+        console.log('The text was appended to the file!'); 
+      }); 
 
 }else{
     console.log('no - dotenv');
+    fs.appendFile('example.txt', 'MISSING DOTENV.', (err) => { 
+        if (err) throw err; 
+        console.log('The text was appended to the file!'); 
+      }); 
 
 }
 console.log('--------END-------');
